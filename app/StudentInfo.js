ee33ac96ea013a2d45b68e12915114e85fee0cbe
simplyekom <simplyekom@gmail.com>
2023-10-02 18:00:38 -0600

    web development-page.js

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function MyComp(){
+    return(
+        <div>
+            <h2> Ekom Uffort</h2>
+            <p> CPRG 306</p>
+            <Link href="https://github.com/simplyekom">My GitHub Repository</Link>
+            
+        </div>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+import MyComp from "../StudentInfo";
+
+export default function Page(){
+    return (
+        <main>
+            <h1>My Shopping List</h1>
+            <MyComp />
+            <Link href="../">&lt;- Back</Link>
+        </main>
+    )
+}
